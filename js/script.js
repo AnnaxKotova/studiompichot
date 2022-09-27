bd59015f4d8cd9429edb6df83cbe3ba8a68fb2c1
Anna <a.kotova@tx-consulting.com>
2022-09-27 16:42:59 +0200

    feat: integration mobile header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let menuBurger = document.getElementById("menu-burger");
+let closeMenu = document.getElementById("closeMenu");
 let navbar = document.getElementById("navbar");
 
 
@@ -10,4 +11,21 @@ import {
 
 if(menuBurger){
 menuBurger.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
+
+
+    let sections = document.querySelectorAll("section");
+    sections.forEach(section => {
+        section.addEventListener('click', function (event) {
+
+            if (navbar.classList.contains("navbar-show")) {
+                let isClickInsideElement = navbar.contains(event.target);
+                if ((!isClickInsideElement)) {
+                    //Do something click is outside specified element
+                    toggleMenu();
+                }
+            }
+        });
+    });
+
 }
