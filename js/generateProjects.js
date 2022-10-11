d6dc9bdc53b81556d00cf636cf76f7f2bf0d2321
Anna <a.kotova@tx-consulting.com>
2022-10-11 15:19:16 +0200

    fix: tag activation onClick

--- js/generateProjects.js
+++ js/generateProjects.js
@@ -1,22 +1,44 @@
-export const generateProjects = (json, tagReceived) =>{
+const slugify = (str) =>
+  str
+  .toLowerCase()
+  .trim()
+  .normalize("NFD")
+  .replace(/[\u0300-\u036f]/g, "")
+  .replace(/[\s_-]+/g, "-")
+  .replace(/^-+|-+$/g, "");
+
+export const generateProjects = (json, tagReceived) => {
+  //in case later to use double map for creation new key-value and modification of this value as an array
+  // let latinJson = json.map((item) => ({
+  //   ...item,
+  //   latinTags: item.tags.map((x) => slugify(x)),
+  // }));
   let sortedItems;
 
-  if(tagReceived === "Tout"){
+  if (tagReceived === "Tout") {
     sortedItems = json;
-  } else sortedItems = json.filter(item => item.tags.includes(tagReceived));
-  console.log(tagReceived, sortedItems, json);
-    for (let i = 0; i < sortedItems.length; i++) {
-        let realisationsMain = document.querySelector(".realisations__main");
-        let realisationsTemplate = document.querySelector("#realisations__items").content;    
-        realisationsMain.append(realisationsTemplate.cloneNode(true));
-        const realisationsImage = document.querySelectorAll('.realisations__image');
-        const realisationsTitle = document.querySelectorAll('.realisations__title');
-        realisationsImage[i].setAttribute('src', `${json[i].thumbnail}`);
-        realisationsTitle[i].textContent = json[i].title;
-      }
+  } else
+    sortedItems = json.filter((item) => item.tags.includes(tagReceived));
+  for (let i = 0; i < sortedItems.length; i++) {
+    let realisationsMain = document.querySelector(".realisations__main");
+    let realisationsTemplate = document.querySelector("#realisations__items").content;
+    realisationsMain.append(realisationsTemplate.cloneNode(true));
+    const realisationsImage = document.querySelectorAll(".realisations__image");
+    const realisationsTitle = document.querySelectorAll(".realisations__title");
+    realisationsImage[i].setAttribute("src", `${json[i].thumbnail}`);
+    realisationsTitle[i].textContent = json[i].title;
+  }
+
+  //is it possible to solve this shit easier ???
+
+  document.querySelectorAll(".realisations__tag").forEach((tag) => tag.classList.remove("bold"));
+//asynchronous js 
+  setTimeout(function () {
+    let currentTags = document.querySelectorAll(`.realisations__tag.${slugify(tagReceived)}`);
+    currentTags.forEach(function (tag) {
+      tag.classList.toggle("bold");
+    }, 0);
+  });
 
 
-      //is it possible to solve this shit easier ???
-
-      document.querySelectorAll(".realisations__tag").forEach(tag =>   tag.classList.remove("bold"));
-}
+};
--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ let homepage;
 
 import { toggleMenu, changeColorInterval, headerChange } from "./functions.js";
 
-import {slideToggle} from "./slideToggle.js";
+import { slideToggle } from "./slideToggle.js";
 
-import {generateProjects} from "./generateProjects.js"
+import { generateProjects } from "./generateProjects.js";
 
 if (menuBurger) {
   menuBurger.addEventListener("click", toggleMenu);
@@ -42,37 +42,31 @@ if (homepage) {
 }
 
 let openersQuestions = Array.from(
-  document.getElementsByClassName("faq__open-question"));
+  document.getElementsByClassName("faq__open-question")
+);
 
 if (openersQuestions) {
   openersQuestions.forEach((openerQuestion) => {
     openerQuestion.addEventListener("click", function toggleQuestion(event) {
       // when the function is anonyme, this is undefined...
-      //class "opened" serves for arrow rotation 
+      //class "opened" serves for arrow rotation
       this.parentNode.classList.toggle("opened");
       slideToggle(this.nextElementSibling, 500);
     });
   });
 }
 
+fetch("../js/projects.json")
+  .then((res) => res.json())
+  .then((json) => {
+    let tagName = "Tout";
+    generateProjects(json, tagName);
 
-
-fetch('../js/projects.json').then(res => res.json()).then(json => {
-  let tagName = "Tout";
-  generateProjects(json, tagName);
-
-
-  let tags = document.querySelectorAll(".realisations__tag");
-  tags.forEach(tag => {
-
-    tag.addEventListener('click', function(event) {
-      // tag.parentNode.childNodes.classList.remove("bold");
-      // {neighbour.classList.remove("bold")}
-      // tag.parentNode.children.forEach(a => console.log(a));
-
-    document.querySelector(".realisations__main").replaceChildren([]);
-    generateProjects(json, tag.textContent);
-    tag.classList.add("bold");
-});
+    let tags = document.querySelectorAll(".realisations__tag");
+    tags.forEach((tag) => {
+      tag.addEventListener("click", function (event) {
+        document.querySelector(".realisations__main").replaceChildren([]);
+        generateProjects(json, tag.textContent);
+      });
     });
-});
+  });
